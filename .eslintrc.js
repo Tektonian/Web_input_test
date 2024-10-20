9939b0234544ba9ff26c4ad9d0f7d80359d26fdb
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-10-20 16:47:36 +0900

    set eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+module.exports = {
+  extends: [ 
+    "@rushstack/eslint-config/profile/node",
+    "@rushstack/eslint-config/mixins/react",
+  ],  // <---- put your profile string here
+  parserOptions: { tsconfigRootDir: __dirname },
+  settings: {
+    react: {
+      "version": "16.9" // <----
+    }
+  }
+};
