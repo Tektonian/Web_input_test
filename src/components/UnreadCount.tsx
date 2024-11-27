ff7f688d38fea5d5d0fe4989718cb1aff29918f0
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-27 13:05:23 +0900

    Update UnreadCount.tsx
    url 주소 수정

--- src/components/UnreadCount.tsx
+++ src/components/UnreadCount.tsx
@@ -8,8 +8,24 @@ interface UnreadCountProps {
 const UnreadCount = ({ onClick }: UnreadCountProps) => {
     const [unreadCount, setUnreadCount] = useState(0);
 
+    /*
+    const { mutate, data, isSuccess } = useMutation({
+        mutationFn: async () => {
+            const res = await fetch("/api/message/unread", {
+                method: "post",
+                credentials: "include",
+                headers: { "Content-Type": "application/json" },
+            });
+            return res.json();
+        },
+        onSuccess: (data, variables, context) => {
+            setUnreadCount(data.unreadCount);
+        },
+    });
+    */
+
     useEffect(() => {
-        const eventSource = new EventSource("http://localhost:8080/api/sse", {
+        const eventSource = new EventSource("/api/sse", {
             withCredentials: true,
         });
         console.log("eventSource: ", eventSource);
@@ -28,7 +44,7 @@ const UnreadCount = ({ onClick }: UnreadCountProps) => {
     return (
         <Box>
             <IconButton onClick={onClick}>
-                <Badge badgeContent={unreadCount}>
+                <Badge badgeContent={unreadCount ?? 0}>
                     <Mail sx={{ fontSize: "xl" }} />
                 </Badge>
             </IconButton>
