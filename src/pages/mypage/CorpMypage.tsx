8d8501a6a64b6274b869bfdf43173597289a287b
Min Jeong <jungm8453@gmail.com>
2024-12-29 09:51:15 +0900

    Student, Corp 마이페이지

--- src/pages/mypage/CorpMypage.tsx
+++ src/pages/mypage/CorpMypage.tsx
@@ -1,29 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { Box, Container, Typography } from "@mui/material";
 import {
-    StudentProfileCard,
-    IndexCard,
+    Box,
+    Container,
+    Tab,
+    Tabs,
+    Typography,
+    Grid2 as Grid,
+    Modal,
+} from "@mui/material";
+import {
+    CorpIndexCard,
     RequestCard,
     CorpProfileCard,
+    ReviewOfCorpCard,
+    ReviewOfCorpInput,
 } from "web_component";
 import { APIType } from "api_spec";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useSession } from "src/hooks/Session";
 
 const CorpMypage = () => {
-    const [
-        corpData,
-        setCorpData,
-    ] = useState<APIType.CorporationType.ResGetCorpProfile | null>(null);
-    const corp_id = useParams();
+    const navigate = useNavigate();
+    const [corpData, setCorpData] =
+        useState<APIType.CorporationType.ResGetCorpProfile | null>(null);
+    const [open, setOpen] = React.useState(false);
+    const [tabIndex, setTabIndex] = useState(0);
+
+    const { data: session, status } = useSession();
+    const name = session?.user?.name || "";
+
+    const { control, handleSubmit } =
+        useForm<APIType.CorpReviewType.ReqCreateCorpReview>({
+            defaultValues: {
+                review_text: "",
+                prep_requirement: "",
+                work_atmosphere: "",
+                sense_of_achive: -1,
+            },
+        });
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabIndex(newValue);
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`/api/corporations/${corp_id}`, {
+                const response = await fetch(`/api/corporations/`, {
                     method: "GET",
                 });
 
-                const data: APIType.CorporationType.ResGetCorpProfile = await response.json();
+                const data: APIType.CorporationType.ResGetCorpProfile =
+                    await response.json();
 
                 console.log(data);
 
@@ -32,6 +61,7 @@ const CorpMypage = () => {
                 console.error("Error fetching corporation data", error);
             }
         };
+        fetchData(); // eslint-disable-line
     }, []);
 
     const ongoingRequests = corpData?.requests.filter(
@@ -43,6 +73,29 @@ const CorpMypage = () => {
     const pastRequests = corpData?.requests.filter(
         (req) => req.request_status === 4 || req.request_status === 5,
     );
+
+    const onSubmit = async (
+        data: APIType.CorpReviewType.ReqCreateCorpReview,
+    ) => {
+        try {
+            const corpRevRes = await fetch(
+                "http://localhost:8080/api/student-reviews",
+                {
+                    method: "POST",
+                    credentials: "include",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(data),
+                },
+            );
+
+            if (corpRevRes.ok) {
+                console.log("Review about Corporation submitted successfully.");
+                setOpen(false);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     const sections = ["0", "1", "2", "3", "4"];
 
@@ -57,6 +110,7 @@ const CorpMypage = () => {
                 maxWidth: "1080px",
                 margin: "auto",
                 padding: "16px",
+                minHeight: "100vh",
             }}
             id={sections[0]}
         >
@@ -68,59 +122,128 @@ const CorpMypage = () => {
             >
                 {corpData && <CorpProfileCard {...corpData?.corp} />}
 
-                <Box sx={{ marginTop: "24px" }} id={sections[1]}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
-                    >
-                        진행 중인 요청
-                    </Typography>
-                    {ongoingRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={false}
-                                onClick={() => alert("ongoing request clicked")}
-                            />
-                        </Box>
-                    ))}
-                </Box>
-
-                <Box sx={{ marginTop: "24px" }} id={sections[4]}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
-                    >
-                        신청 요청
-                    </Typography>
-                    {openRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={false}
-                                onClick={() => alert("open request clicked")}
-                            />
-                        </Box>
-                    ))}
-                </Box>
-
                 <Box sx={{ marginTop: "24px" }}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
+                    <Tabs
+                        value={tabIndex}
+                        onChange={handleTabChange}
+                        centered
+                        variant="fullWidth"
                     >
-                        과거 요청
-                    </Typography>
-                    {pastRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={false}
-                                onClick={() => alert("past request clicked")}
-                            />
-                        </Box>
-                    ))}
+                        <Tab label="의뢰" />
+                        <Tab label="리뷰" />
+                    </Tabs>
                 </Box>
+
+                {tabIndex === 0 && (
+                    <>
+                        <Box sx={{ marginTop: "24px" }} id={sections[1]}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                진행 중인 요청
+                            </Typography>
+                            {ongoingRequests?.map((request, index) => (
+                                <Box key={index} sx={{ marginTop: "16px" }}>
+                                    <RequestCard
+                                        {...request}
+                                        address={request.address ?? ""}
+                                        request_status={
+                                            request.request_status ?? 3
+                                        }
+                                        renderLogo={false}
+                                        onClick={() =>
+                                            alert("ongoing request clicked")
+                                        }
+                                    />
+                                </Box>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ marginTop: "24px" }} id={sections[4]}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                신청 요청
+                            </Typography>
+                            {openRequests?.map((request, index) => (
+                                <Box key={index} sx={{ marginTop: "16px" }}>
+                                    <RequestCard
+                                        {...request}
+                                        address={request.address ?? ""}
+                                        request_status={
+                                            request.request_status ?? 0
+                                        }
+                                        renderLogo={false}
+                                        onClick={() =>
+                                            navigate(
+                                                `/request/${request.request_id}`,
+                                            )
+                                        }
+                                    />
+                                </Box>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ marginTop: "24px" }}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                과거 요청
+                            </Typography>
+                            {pastRequests?.map((request, index) => (
+                                <>
+                                    <Box key={index} sx={{ marginTop: "16px" }}>
+                                        <RequestCard
+                                            {...request}
+                                            address={request.address ?? ""}
+                                            request_status={
+                                                request.request_status ?? 4
+                                            }
+                                            renderLogo={false}
+                                            onClick={() => setOpen(true)}
+                                        />
+                                    </Box>
+                                    <Modal
+                                        open={open}
+                                        onClose={() => setOpen(false)}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                    >
+                                        <ReviewOfCorpInput
+                                            control={control}
+                                            onSubmit={handleSubmit(onSubmit)}
+                                            onCancel={() => setOpen(false)}
+                                        />
+                                    </Modal>
+                                </>
+                            ))}
+                        </Box>
+                    </>
+                )}
+
+                {tabIndex === 1 && (
+                    <Box sx={{ marginTop: "16px" }}>
+                        <Grid container spacing={3}>
+                            {corpData?.reviews.map((review, index) => (
+                                <Grid size={6} key={index}>
+                                    <ReviewOfCorpCard {...review} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                )}
             </Container>
 
             <Container
@@ -133,7 +256,7 @@ const CorpMypage = () => {
                     order: { xs: -1, md: 1 }, // 모바일에서 위로 이동
                 }}
             >
-                <IndexCard roles="corp" sections={sections} />
+                <CorpIndexCard />
             </Container>
         </Box>
     );
--- src/pages/mypage/StudentMypage.tsx
+++ src/pages/mypage/StudentMypage.tsx
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Typography } from "@mui/material";
-import { StudentProfileCard, IndexCard, RequestCard } from "web_component";
+import {
+    Box,
+    Container,
+    Tab,
+    Tabs,
+    Typography,
+    Grid2 as Grid,
+    Modal,
+} from "@mui/material";
+import {
+    StudentProfileCard,
+    StudentIndexCard,
+    RequestCard,
+    ReviewOfStudentCard,
+    ReviewOfCorpInput,
+} from "web_component";
 import { APIType } from "api_spec";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useSession } from "src/hooks/Session";
 
 const StudentMypage = () => {
-    const [
-        studentData,
-        setStudentData,
-    ] = useState<APIType.StudentType.ResGetStudentProfile | null>(null);
-    const student_id = useParams();
     const navigate = useNavigate();
+    const [studentData, setStudentData] =
+        useState<APIType.StudentType.ResGetStudentProfile | null>(null);
+    const [open, setOpen] = React.useState(false);
+    const [tabIndex, setTabIndex] = useState(0);
 
+    const { data: session, status } = useSession();
+    const name = session?.user?.name || "";
+
+    const { control, handleSubmit } =
+        useForm<APIType.CorpReviewType.ReqCreateCorpReview>({
+            defaultValues: {
+                review_text: "",
+                prep_requirement: "",
+                work_atmosphere: "",
+                sense_of_achive: -1,
+            },
+        });
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabIndex(newValue);
+    };
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`api/students/${student_id}`, {
-                    method: "GET",
-                });
-                const data: APIType.StudentType.ResGetStudentProfile = await response.json();
+                const response = await fetch(
+                    `http://localhost:8080/api/students/mypage?${name}`,
+                    {
+                        method: "GET",
+                    },
+                );
+                const data: APIType.StudentType.ResGetStudentProfile =
+                    await response.json();
 
                 console.log(data);
 
@@ -27,11 +62,10 @@ const StudentMypage = () => {
                 console.error("Error fetching student data", error);
             }
         };
+        fetchData(); // eslint-disable-line
     }, []);
 
     const handleSendingAlarm = () => {};
-
-    const handleRenderReview = () => {};
 
     const ongoingRequests = studentData?.requests.filter(
         (req) => req.request_status === 3,
@@ -42,6 +76,29 @@ const StudentMypage = () => {
     const pastRequests = studentData?.requests.filter(
         (req) => req.request_status === 4 || req.request_status === 5,
     );
+
+    const onSubmit = async (
+        data: APIType.CorpReviewType.ReqCreateCorpReview,
+    ) => {
+        try {
+            const corpRevRes = await fetch(
+                "http://localhost:8080/api/corporation-reviews",
+                {
+                    method: "POST",
+                    credentials: "include",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(data),
+                },
+            );
+
+            if (corpRevRes.ok) {
+                console.log("Review about Corporation submitted successfully.");
+                setOpen(false);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     const sections = ["0", "1", "2", "3", "4"];
 
@@ -56,74 +113,145 @@ const StudentMypage = () => {
                 maxWidth: "1080px",
                 margin: "auto",
                 padding: "16px",
+                minHeight: "100vh",
             }}
             id={sections[0]}
         >
-            <Container sx={{ width: "712px", padding: "0 !important" }}>
+            <Container
+                sx={{
+                    width: { xs: "100%", md: "712px" },
+                    padding: "0 !important",
+                }}
+            >
                 {studentData && (
                     <StudentProfileCard
                         {...studentData.profile}
                         student_name={JSON.stringify(
                             studentData?.profile.name_glb,
                         )}
+                        isMypage={true}
                     />
                 )}
 
-                <Box sx={{ marginTop: "24px" }} id={sections[1]}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
-                    >
-                        도착 알람을 보내세요!
-                    </Typography>
-                    {ongoingRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={true}
-                                onClick={handleSendingAlarm}
-                            />
-                        </Box>
-                    ))}
-                </Box>
-
-                <Box sx={{ marginTop: "24px" }} id={sections[4]}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
-                    >
-                        신청 요청
-                    </Typography>
-                    {openRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={true}
-                                onClick={() =>
-                                    navigate(`/request/${request.request_id}`)
-                                }
-                            />
-                        </Box>
-                    ))}
-                </Box>
-
                 <Box sx={{ marginTop: "24px" }}>
-                    <Typography
-                        variant="h6"
-                        sx={{ fontWeight: "bold", marginBottom: "16px" }}
+                    <Tabs
+                        value={tabIndex}
+                        onChange={handleTabChange}
+                        centered
+                        variant="fullWidth"
                     >
-                        과거 요청
-                    </Typography>
-                    {pastRequests?.map((request, index) => (
-                        <Box key={index} sx={{ marginTop: "16px" }}>
-                            <RequestCard
-                                {...request}
-                                renderLogo={true}
-                                onClick={handleRenderReview}
-                            />
-                        </Box>
-                    ))}
+                        <Tab label="의뢰" />
+                        <Tab label="리뷰" />
+                    </Tabs>
                 </Box>
+
+                {tabIndex === 0 && (
+                    <>
+                        <Box sx={{ marginTop: "24px" }} id={sections[1]}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                도착 알람을 보내세요!
+                            </Typography>
+                            {ongoingRequests?.map((request, index) => (
+                                <Box key={index} sx={{ marginTop: "16px" }}>
+                                    <RequestCard
+                                        {...request}
+                                        address={request.address ?? ""}
+                                        request_status={
+                                            request.request_status ?? 3
+                                        }
+                                        renderLogo={true}
+                                        onClick={handleSendingAlarm}
+                                    />
+                                </Box>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ marginTop: "24px" }} id={sections[4]}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                신청 요청
+                            </Typography>
+                            {openRequests?.map((request, index) => (
+                                <Box key={index} sx={{ marginTop: "16px" }}>
+                                    <RequestCard
+                                        {...request}
+                                        address={request.address ?? ""}
+                                        request_status={
+                                            request.request_status ?? 0
+                                        }
+                                        renderLogo={true}
+                                        onClick={() =>
+                                            navigate(
+                                                `/request/${request.request_id}`,
+                                            )
+                                        }
+                                    />
+                                </Box>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ marginTop: "24px" }}>
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    fontWeight: "bold",
+                                    marginBottom: "16px",
+                                }}
+                            >
+                                과거 요청
+                            </Typography>
+                            {pastRequests?.map((request, index) => (
+                                <>
+                                    <Box key={index} sx={{ marginTop: "16px" }}>
+                                        <RequestCard
+                                            {...request}
+                                            address={request.address ?? ""}
+                                            request_status={
+                                                request.request_status ?? 4
+                                            }
+                                            renderLogo={true}
+                                            onClick={() => setOpen(true)}
+                                        />
+                                    </Box>
+                                    <Modal
+                                        open={open}
+                                        onClose={() => setOpen(false)}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                    >
+                                        <ReviewOfCorpInput
+                                            control={control}
+                                            onSubmit={handleSubmit(onSubmit)}
+                                            onCancel={() => setOpen(false)}
+                                        />
+                                    </Modal>
+                                </>
+                            ))}
+                        </Box>
+                    </>
+                )}
+                {tabIndex === 1 && (
+                    <Box sx={{ marginTop: "16px" }}>
+                        <Grid container spacing={3}>
+                            {studentData?.reviews.map((review, index) => (
+                                <Grid size={6} key={index}>
+                                    <ReviewOfStudentCard {...review} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                )}
             </Container>
 
             <Container
@@ -136,7 +264,7 @@ const StudentMypage = () => {
                     order: { xs: -1, md: 1 }, // 모바일에서 위로 이동
                 }}
             >
-                <IndexCard roles="student" sections={sections} />
+                <StudentIndexCard sections={sections} />
             </Container>
         </Box>
     );
